0e73e8d278ec7afa70fa412bb26167fc3d47a6cd
ivanaagwuye <ivancollev2@gmail.com>
2022-10-20 19:50:45 +0100

    hey

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 
 /**
 * _isalpha - checks for alphabetic character
-* @c: checks for the alphabet
+* @c: character to be checked
 * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
 */
 
