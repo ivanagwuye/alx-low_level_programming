28290bf7106981cccd76f1ee07f01fe1ab8afa64
ivanaagwuye <ivancollev2@gmail.com>
2022-10-23 18:04:00 +0100

    hot fix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - prints the statement
+*
+* Return: 1
+*/
+
+int main(void)
+{
+	write ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return(1);
+}
