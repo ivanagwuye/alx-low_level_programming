967e5c51e360e6a839eb27193c8124cb8893f071
ivanaagwuye <ivancollev2@gmail.com>
2022-10-24 21:16:40 +0100

    hot fix

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - print all single digit numbers of base 10
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+	putchar(c);
+	}
+	putchar('\n');
+
+	return(0);
+}
