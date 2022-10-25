961ef164c83a5f4bcc463080d55cdc697207fa4f
ivanaagwuye <ivancollev2@gmail.com>
2022-10-25 01:23:55 +0100

    hot fix

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,13 +2,15 @@
 
 /**
 * add - function that adds two integers, and returns the result
-*
+* @a: first integer to be checked
+* @b: second integer to be checked
 * Return: the result of the addition
 */
 
 int add(int a, int b)
 {
 	int RESULT;
+
 	RESULT = a + b;
 
 	return (RESULT);
