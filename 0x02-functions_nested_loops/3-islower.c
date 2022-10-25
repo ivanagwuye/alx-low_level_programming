65f743039bb7c9bf8df4819ed9e67ed265a993e3
ivanaagwuye <ivancollev2@gmail.com>
2022-10-25 22:19:29 +0100

    hot fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* _islo - checks for lower case character
+* _islower - checks for lower case character
 * @c: the character to check
 * Return: 1 if c is lowercase, 0 otherwise
 */
