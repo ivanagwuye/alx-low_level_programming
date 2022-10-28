2a86d682e3195d31bc0b1bd98380e776ac2b3335
ivanaagwuye <ivancollev2@gmail.com>
2022-10-28 11:26:54 +0100

    hot fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _strcat - concatenate two strings
- * @dest: string to be appended to
- * @src: string to append
- * Return: concatenated string
- */
+* _strcat - concatenate two strings
+* @dest: string to be appended to
+* @src: string to append
+* Return: concatenated string
+*/
 
 char *_strcat(char *dest, char *src)
 {
@@ -26,24 +26,23 @@ char *_strcat(char *dest, char *src)
 	return (dest);
 }
 
-/* another method using pointer notation
-
-char *_strcat(char *dest, char *src)
-{
-        int i = 0;
-	int j = 0;
-
-	while (*(dest + i))
-	   i++;
-
-	while (*(src + j))
-	{
-	   *(dest+i) = *(src+j);
-	   i++;
-	   j++;
-	}
-	*(dest+i) = '\0';
-
-	return (dest);
-}
+/**
+* another method using pointer notation
+*
+* char *_strcat(char *dest, char *src)
+* {
+*        int i = 0;
+*	int j = 0;
+*	while (*(dest + i))
+*	   i++;
+*
+*	while (*(src + j))
+*	{
+*	   *(dest+i) = *(src+j);
+*	   i++;
+*	   j++;
+*	}	*(dest+i) = '\0';
+*
+*	return (dest);
+* }
 */
