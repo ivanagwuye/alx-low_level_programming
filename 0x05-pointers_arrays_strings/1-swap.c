c8be998337e4813958960c18e5926c5cba215638
ivanaagwuye <ivancollev2@gmail.com>
2022-10-28 08:28:32 +0100

    hot fix

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -10,14 +10,8 @@
 
 void swap_int(int *a, int *b)
 {
-	int a;
-	int b;
-
-	a = 98;
-	b = 42;
-	printf("a=%d, b=%d\n", a, b);
-	swap_int(&a, &b)
-	printf("a=%d, b=%d\n", a, b);
-
-	return (0);
+	int c;
+	*a = c;
+	*a = *b;
+	*b = c;
 }
